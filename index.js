4dd8f44707753ab79355bc5c9b8ade13d11dccf5
Iucism <105166639+Iucism@users.noreply.github.com>
2022-07-18 22:13:12 -0400

    format

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-const { Plugin } = require('powercord/entities');
-const { findInReactTree } = require('powercord/util');
-const { inject, uninject } = require('powercord/injector');
-const { React, getModule } = require('powercord/webpack');
+const { Plugin } = require("powercord/entities");
+const { findInReactTree } = require("powercord/util");
+const { inject, uninject } = require("powercord/injector");
+const { React, getModule } = require("powercord/webpack");
 
-
-
-module.exports = class QuickEmoji  extends Plugin {
-  async startPlugin () {
+module.exports = class QuickEmoji extends Plugin {
+  async startPlugin() {
     const classes = {
-      ...await getModule([ 'icon', 'isHeader' ]),
-      ...await getModule([ 'button', 'separator', 'wrapper' ])    
+      ...(await getModule(["icon", "isHeader"])),
+      ...(await getModule(["button", "separator", "wrapper"])),
     };
-    const reactionManager = await getModule([ 'addReaction' ]);
-    const MiniPopover = await getModule(m => m.default && m.default.displayName === 'MiniPopover');
+    const reactionManager = await getModule(["addReaction"]);
+    const MiniPopover = await getModule((m) => m.default && m.default.displayName === "MiniPopover");
     const emojis = [
       {
-        name: '❤️',
-        url: '/assets/0483f2b648dcc986d01385062052ae1c.svg'
+        name: "❤️",
+        url: "/assets/0483f2b648dcc986d01385062052ae1c.svg",
       },
     ];
 
-
-    inject('heart-button', MiniPopover, 'default', (_, res) => {
-      const props = findInReactTree(res, r => r && r.canReact && r.message);
-      if (!props || props.message.reactions.some(r => emojis.some(e => r.emoji.name === e.name) && r.me)) {
+    inject("emoji-button", MiniPopover, "default", (_, res) => {
+      const props = findInReactTree(res, (r) => r && r.canReact && r.message);
+      if (!props || props.message.reactions.some((r) => emojis.some((e) => r.emoji.name === e.name) && r.me)) {
         return res;
       }
 
-      res.props.children.unshift(...emojis.map(emoji =>
-        React.createElement(
-            'div', {
-            key: emoji.name,
-            className: classes.button,
-            onClick: () => reactionManager.addReaction(props.channel.id, props.message.id, {
-              animated: false,
-              name: emoji.name,
-              id: null
+      res.props.children.unshift(
+        ...emojis.map((emoji) =>
+          React.createElement(
+            "div",
+            {
+              key: emoji.name,
+              className: classes.button,
+              onClick: () =>
+                reactionManager.addReaction(props.channel.id, props.message.id, {
+                  animated: false,
+                  name: emoji.name,
+                  id: null,
+                }),
+            },
+            React.createElement("img", {
+              className: `emoji ${classes.icon}`,
+              src: emoji.url,
             })
-          },
-          React.createElement('img', {
-            className: `emoji ${classes.icon}`,
-            src: emoji.url, 
-          })
+          )
         )
-      ));
+      );
       return res;
     });
-    MiniPopover.default.displayName = 'MiniPopover';
+    MiniPopover.default.displayName = "MiniPopover";
   }
 
-  pluginWillUnload () {
-    uninject('heart-button');
+  pluginWillUnload() {
+    uninject("emoji-button");
   }
 };
